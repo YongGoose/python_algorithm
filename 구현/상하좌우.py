758c4b9d9bb7ecd5d19ab01dea071de8caee56ab
hong <kevin0928@naver.com>
2023-02-07 22:20:42 +0900

    구현

--- 구현/상하좌우.py
+++ 구현/상하좌우.py
@@ -5,7 +5,7 @@ plans = input().split()
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
 move_types = ['L','R','U','D']
-
+ 
 for plan in plans:
     for i in range(len(move_types)):
         if plan == move_types[i]:
