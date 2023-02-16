b2aabccfdc6b6a437e6e67e8018d9872ae10e847
hong <kevin0928@naver.com>
2023-02-16 19:55:46 +0900

    baekjoon sort

---
+++ 백준/정렬/회의실 배정.py
@@ -0,0 +1,21 @@
+num = int(input())
+classroom = []
+
+for i in range(num):
+    input_data = input().split()
+    classroom.append((int(input_data[0]), int(input_data[1])))
+
+classroom = sorted(classroom,key=lambda classroom : classroom[0])
+classroom = sorted(classroom,key=lambda classroom : classroom[1])
+
+# print(classroom,"\n")
+num_of_class = 0
+
+end = 99999999999999999999999
+for i in classroom:
+    if i[0] >= end or num_of_class == 0:
+        end = i[1]
+        # print(i)
+        num_of_class += 1
+
+print(num_of_class)
