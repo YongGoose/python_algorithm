ce4d44a2988e76fea1ae7ea23375dab6405921a4
YongJun <kevin0928@naver.com>
2023-06-23 00:25:23 +0900

    [Silver III] Title: 회전하는 큐, Time: 68 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,35 +1,33 @@
 from collections import deque
-import sys
 
-input = sys.stdin.readline
-
-num, times = map(int,input().split())
+num, times = map(int, input().split())
 num_list = deque([(i+1) for i in range(num)])
 
-mission_list = list(map(int,input().split()))
+mission_list = list(map(int, input().split()))
 
 count = 0
-change_num, index, middle_number = 0,0, num // 2
+change_num, index, middle_number = 0, 0, num // 2
 
 for i in range(times):
-    for e in range(num):
-        if(num_list[e] == mission_list[i]):
-            index = e
-    if(middle_number >= index):
-        while (num_list[0] != mission_list[i]):
+    for j in range(num):
+        if num_list[j] == mission_list[i]:
+            index = j
+    
+    # 덱의 중간 위치를 기준으로 이동 방향을 결정합니다.
+    if middle_number >= index:
+        while num_list[0] != mission_list[i]:
             change_num = num_list.popleft()
             num_list.append(change_num)
             count += 1
     else:
-        while (num_list[0] != mission_list[i]):        
+        while num_list[0] != mission_list[i]:        
             change_num = num_list.pop()
             num_list.appendleft(change_num)
             count += 1
         
-    if(num_list[0] == mission_list[i]):
+    if num_list[0] == mission_list[i]:
         num_list.popleft()
         num -= 1
         middle_number = num // 2
 
 print(count)
-        
